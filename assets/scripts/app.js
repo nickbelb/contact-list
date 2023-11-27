c0fe17ad9e8b3c1cb776b801f866a5e4153285b4
Nicolas Beltran <nicolasfelipebeltran@gmail.com>
2023-11-27 09:57:29 -0600

    bug fixes

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -11,14 +11,15 @@ const messagBox = utils.selectObject('.message');
 const main = utils.selectObject('.main');
 const yesBtn= utils.selectObject('.yes-option');
 const noBtn= utils.selectObject('.no-option');
+let indexObjt;
 let contactList=[];
 let contactListDivs=[];
 let contactInfo=[];
 
 function validateContactInfo(){
     contactInfo = (contactDetails.value).split(',');
-    if(contactInfo.length==3){
-        let validationRegex=/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if(contactInfo.length == 3){
+        let validationRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
         if(validationRegex.test(contactInfo[2])){
             return true;
         }
@@ -37,19 +38,14 @@ function makeVisible(index){
     confirmationBox.classList.remove('hidden');
     confirmationBox.classList.add('visible')
     main.classList.add('opacity-visible');
-    utils.onEvent('click',yesBtn,function() {
-        deleteObj(index);
-    });
-    utils.onEvent('click',noBtn,function() {
-        makeInvisible();
-    });
+    indexObjt = index;
 }
 
 function deleteObj(index) {
-contactListDivs.splice(index,1);
-contactList.splice(index,1);
-showListDivs();
-makeInvisible();
+    makeInvisible();
+    contactListDivs.splice(index,1);
+    contactList.splice(index,1);
+    showListDivs();
 }
 
 function showListDivs(){
@@ -59,28 +55,34 @@ function showListDivs(){
     });
 }
 
+function createContactDiv(contact){
+    const contactBox = document.createElement('div');
+    const contactData = document.createElement('p');
+    const deleteBtn = document.createElement('i');
+    let message = `Name: ${contact.name}\n City: ${contact.city}\n Email: ${contact.email}`;
+    contactData.innerText = message;
+    deleteBtn.onclick = function(){makeVisible(contactList.indexOf(contact))};
+    utils.addClassToItems(contactBox,'contact-details');
+    utils.addClassToItems(deleteBtn,'fa-solid','fa-trash-can');
+    utils.addChildToElement(contactBox,contactData,deleteBtn);
+    utils.addChildToElement(contactListBox,contactBox);
+    contactListDivs.push(contactBox);
+}
+
 function createContact(){
-    let validate=validateContactInfo();
+    let validate = validateContactInfo();
     if (validate){
         let contact = new Contact(contactInfo[0],contactInfo[2],contactInfo[1]);
         contactList.push(contact);
-        const contactBox= document.createElement('div');
-        const contactData= document.createElement('p');
-        const deleteBtn= document.createElement('i');
-        let message = `Name: ${contact.name}\n City: ${contact.city}\n Email: ${contact.email}`;
-        contactData.innerText=message;
-        deleteBtn.onclick = function(){makeVisible(contactList.indexOf(contact))};
-        utils.addClassToItems(contactBox,'contact-details');
-        utils.addClassToItems(deleteBtn,'fa-solid','fa-trash-can');
-        utils.addChildToElement(contactBox,contactData,deleteBtn);
-        utils.addChildToElement(contactListBox,contactBox);
-        contactListDivs.push(contactBox);
+        createContactDiv(contact);
     }else{
         let message = `Incorrect format(must be name,city,email) or incorrect email format`;
-        messagBox.textContent=message;
+        messagBox.textContent = message;
     }
 }
 
 utils.onEvent('click',addBtn,createContact);
+utils.onEvent('click',noBtn,function() {makeInvisible()});
+utils.onEvent('click',yesBtn,function() {deleteObj(indexObjt) });
 
 
